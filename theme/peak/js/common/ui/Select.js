56aa4a0983747ef74e68a34b706c6d10f26a6422
zhangxinxu <zhangxinxu@zhangxinxu.com>
2018-06-27 22:06:40 +0800

    select support multiple
    select support multiple

--- theme/peak/js/common/ui/Select.js
+++ theme/peak/js/common/ui/Select.js
@@ -38,6 +38,7 @@
     var DISABLED = 'disabled';
     var ACTIVE = 'active';
     var REVERSE = 'reverse';
+    var MULTIPLE = 'multiple';
 
     // 默认参数
     var prefix = 'ui-';
@@ -84,12 +85,16 @@
         }
         this.id = id;
 
-        // 2. 创建下拉组合框元素
+        // 2. 是否多选
+        var multiple = (typeof el.attr(MULTIPLE) == 'string');
+        this.multiple = multiple;
+
+        // 3. 创建下拉组合框元素
         var combobox = $('<div></div>').attr({
             role: 'combobox'
         });
 
-        // 3. 创建下拉点击按钮元素
+        // 4. 创建下拉点击按钮元素
         var button = $('<a></a>').attr({
             href: 'javascript:',
             'data-target': id,
@@ -99,7 +104,7 @@
             'aria-owns': id
         }).addClass(PREFIX + joiner + 'button');
 
-        // 4. 创建下拉列表元素
+        // 5. 创建下拉列表元素
         var datalist = $('<div></div>').attr({
             id: id,
             // for aria
@@ -107,10 +112,21 @@
             'aria-expanded': 'true'
         }).addClass(PREFIX + joiner + 'datalist');
 
-        // 5. 元素组装
-        combobox.append(button).append(datalist);
-        el.hide().after(combobox);
-
+        // 6. 元素组装
+        // multiple没有button
+        if (!multiple) {
+            combobox.append(button).append(datalist);
+            el.hide().after(combobox);
+        } else {
+            combobox.append(datalist);
+            // 绝对定位隐藏，以便可以响应键盘focus
+            el.css({
+                position: 'absolute',
+                zIndex: 1
+            }).after(combobox);
+            // 视觉列表不参与设置为不可访问
+            datalist.attr('aria-hidden', 'true');
+        }
         // 暴露给其他方法
         this.el = {
             sel: el,
@@ -140,64 +156,120 @@
         var button = el.button;
         var datalist = el.datalist;
 
-        // 点击按钮
-        button.on('click', function () {
-            if (sel.prop(DISABLED)) {
-                return false;
-            }
+        // options元素
+        var elOptions = sel.find('option');
 
-            // 显示与隐藏
-            combobox.toggleClass(ACTIVE);
-
-            if (combobox.hasClass(ACTIVE)) {
-                // 边界判断
-                var overflow = datalist.offset().top + datalist.outerHeight() > Math.max($(document.body).height(), $(window).height());
-                combobox[overflow ? 'addClass' : 'removeClass'](REVERSE);
-                // aria状态
-                button.attr('aria-expanded', 'true');
-
-                // 滚动与定位
-                var arrData = combobox.data('scrollTop');
-                var selected = datalist.find('.' + SELECTED);
-                // 严格验证
-                if (arrData && arrData[1] == selected.attr('data-index') && arrData[2] == selected.text()) {
-                    datalist.scrollTop(arrData[0]);
-                    // 重置
-                    combobox.removeData('scrollTop');
+        // 单选下拉框的事件
+        if (!this.multiple) {
+            // 点击按钮
+            button.on('click', function () {
+                if (sel.prop(DISABLED)) {
+                    return false;
                 }
-            } else {
-                combobox.removeClass(REVERSE);
-                // aria状态
+
+                // 显示与隐藏
+                combobox.toggleClass(ACTIVE);
+
+                if (combobox.hasClass(ACTIVE)) {
+                    // 边界判断
+                    var overflow = datalist.offset().top + datalist.outerHeight() > Math.max($(document.body).height(), $(window).height());
+                    combobox[overflow ? 'addClass' : 'removeClass'](REVERSE);
+                    // aria状态
+                    button.attr('aria-expanded', 'true');
+
+                    // 滚动与定位
+                    var arrData = combobox.data('scrollTop');
+                    var selected = datalist.find('.' + SELECTED);
+                    // 严格验证
+                    if (arrData && arrData[1] == selected.attr('data-index') && arrData[2] == selected.text()) {
+                        datalist.scrollTop(arrData[0]);
+                        // 重置
+                        combobox.removeData('scrollTop');
+                    }
+                } else {
+                    combobox.removeClass(REVERSE);
+                    // aria状态
+                    button.attr('aria-expanded', 'false');
+                }
+            });
+
+            // 点击列表项
+            datalist.on('click', 'a[data-index]', function() {
+                var indexOption = $(this).attr('data-index');
+                var scrollTop = datalist.scrollTop();
+                // 存储可能的滚动定位需要的数据
+                combobox.data('scrollTop', [scrollTop, indexOption, $(this).text()]);
+                // 修改下拉选中项
+                elOptions[indexOption][SELECTED] = true;
+                // 下拉收起
+                combobox.removeClass(ACTIVE);
                 button.attr('aria-expanded', 'false');
-            }
-        });
+                // focus
+                button.focus().blur();
+                // 更新下拉框
+                self.refresh();
+                // 回调处理
+                sel.trigger('change');
+            });
 
-        // 点击列表项
-        datalist.on('click', 'a[data-index]', function() {
-            var indexOption = $(this).attr('data-index');
-            var scrollTop = datalist.scrollTop();
-            // 下拉收起
-            combobox.removeClass(ACTIVE);
-            button.attr('aria-expanded', 'false');
-            // 存储可能的滚动定位需要的数据
-            combobox.data('scrollTop', [scrollTop, indexOption, $(this).text()]);
-            // 修改下拉选中项
-            sel.find('option')[indexOption][SELECTED] = true;
-            // 更新下拉框
-            self.refresh();
-            // 回调处理
-            sel.trigger('change');
-            // focus
-            button.focus().blur();
-        });
+            // 点击页面空白要隐藏
+            $(document).mouseup(function(event) {
+                var target = event.target;
+                if (target && combobox.hasClass(ACTIVE) && combobox[0] !== target && combobox[0].contains(target) == false) {
+                    combobox.removeClass(ACTIVE).removeClass(REVERSE);
+                }
+            });
+        } else {
+            // 键盘交互UI同步
+            sel.on('change', function () {
+                // 更新下拉框
+                self.refresh();
+            });
+            // 滚动同步
+            sel.on('scroll', function () {
+                datalist.scrollTop(sel.scrollTop());
+            });
+            // hover穿透
+            sel.on('mousedown', function () {
+                sel.data(ACTIVE, 'true');
+            }).on('mousemove', function (event) {
+                if (sel.data(ACTIVE)) {
+                    self.refresh();
+                } else {
+                    var clientY = event.clientY;
+                    // 当前坐标元素
+                    // 最好方法是使用
+                    // document.elementsFromPoint
+                    // IE10+是document.msElementFromPoint
+                    // 但IE8, IE9浏览器并不支持
+                    // 所以这里采用其他方法实现
+                    // 判断列表的y位置和clientY做比较
+                    var elLists = datalist.find('a');
+                    // 所有hover状态先还原
+                    elLists.removeAttr('href');
+                    // 然后开始寻找匹配的列表元素
+                    elLists.each(function () {
+                        // 进行比对
+                        var beginY = this.getBoundingClientRect().top;
+                        var endY = beginY + this.clientHeight;
+                        // 如果在区间范围
+                        if (clientY >= beginY && clientY <= endY) {
+                            if ($(this).hasClass(SELECTED) == false) {
+                                this.href = 'javascript:';
+                            }
+                            // 退出循环
+                            return false;
+                        }
+                    });
+                }
+            }).on('mouseout', function () {
+                datalist.find('a').removeAttr('href');
+            });
 
-        // 点击页面空白要隐藏
-        $(document).mouseup(function(event) {
-            var target = event.target;
-            if (target && combobox.hasClass(ACTIVE) && combobox[0] !== target && combobox[0].contains(target) == false) {
-                combobox.removeClass(ACTIVE).removeClass(REVERSE);
-            }
-        });
+            $(document).mouseup(function() {
+                sel.removeData(ACTIVE);
+            });
+        }
     };
 
     /* 把下拉元素变成数据，格式为：
@@ -240,6 +312,8 @@
         var self = this;
         // 实例id
         var id = self.id;
+        // 是否多选
+        var multiple = self.multiple;
         // 各个元素
         var el = self.el;
         // 主要的几个元素
@@ -253,17 +327,23 @@
 
         // 下拉组合框元素的UI和尺寸
         combobox.attr('class', $.trim(sel[0].className + ' ' + PREFIX)).width(sel.outerWidth());
-        // 按钮元素中的文案
-        button.html('<span class="' + PREFIX + joiner + 'text">' + (function () {
-            var htmlSelected = '';
-            $.each(data, function (i, obj) {
-                if (obj.selected) {
-                    htmlSelected = obj.html;
-                }
-            });
+        // 多选，高度需要同步，因为选项高度不确定
+        if (multiple) {
+            combobox.height(sel.outerHeight());
+        } else {
+            // 按钮元素中的文案
+            button.html('<span class="' + PREFIX + joiner + 'text">' + (function () {
+                var htmlSelected = '';
+                $.each(data, function (i, obj) {
+                    if (obj.selected) {
+                        htmlSelected = obj.html;
+                    }
+                });
 
-            return htmlSelected || data[0].html;
-        })() + '</span><i class="' + PREFIX + joiner + 'icon" aria-hidden="true"></i>');
+                return htmlSelected || data[0].html;
+            })() + '</span><i class="' + PREFIX + joiner + 'icon" aria-hidden="true"></i>');
+        }
+
         // 列表内容的刷新
         datalist.html($.map(data, function (obj, index) {
             var arrCl = [PREFIX + joiner + 'datalist' + joiner + 'li', obj.className];
@@ -275,7 +355,12 @@
                 arrCl.push(DISABLED);
             }
 
-            return '<a href="javascript:" class="' + arrCl.join(' ') + '" data-index=' + index + ' data-target="' + id + '" role="option" aria-selected="' + obj[SELECTED] + '">' + obj.html + '</a>';
+            // 复选模式列表不参与无障碍访问识别，因此HTML相对简单
+            if (multiple) {
+                return '<a class="' + arrCl.join(' ') + '" data-index=' + index + '>' + obj.html + '</a>';
+            }
+
+            return '<a ' + (obj[DISABLED] ? '' : 'href="javascript:" ') + 'class="' + arrCl.join(' ') + '" data-index=' + index + ' data-target="' + id + '" role="option" aria-selected="' + obj[SELECTED] + '">' + obj.html + '</a>';
         }).join(''));
 
         return this;
