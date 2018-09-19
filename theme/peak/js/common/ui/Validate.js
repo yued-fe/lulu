e5ba1da7a78417a520d0d9fe56ce1bf26a29edfe
zhangxinxu <zhangxinxu@zhangxinxu.com>
2018-09-19 16:01:56 +0800

    all inputs paste improve
    all inputs paste improve

--- theme/peak/js/common/ui/Validate.js
+++ theme/peak/js/common/ui/Validate.js
@@ -1270,21 +1270,49 @@
             var attrType = ele.getAttribute('type');
 
             // 手机号码粘贴的优化处理
-            if (attrType == 'tel' && ele.addEventListener) {
+            if (ele.addEventListener && /^checkbox|radio|range$/i.test(attrType) == false) {
                 ele.addEventListener('paste', function (event) {
+                    // 剪切板数据对象
+                    // 输入框类型
+                    var type = this.getAttribute('type') || this.type;
                     // 剪切板数据对象
                     var clipboardData = event.clipboardData || window.clipboardData;
                     // 粘贴内容
                     var paste = '';
-                    // 且光标是可输入状态
-                    if (clipboardData && $.trim(this.value) == '') {
+                    // 剪切板对象可以获取
+                    if (!clipboardData) {
+                        return;
+                    }
+                    // 获取选中的文本内容
+                    var textSelected = '';
+                    if (window.getSelection) {
+                        // 现代浏览器
+                        // 直接window.getSelection().toString()对于IE的输入框无效
+                        textSelected = this.value.slice(ele.selectionStart, ele.selectionEnd);
+                    } else if (document.selection) {
+                        // 旧IE浏览器
+                        textSelected = document.selection.createRange().text;
+                    }
+                    // 只有输入框没有数据，或全选状态才处理
+                    if (this.value.trim() == '' || textSelected === this.value) {
                         // 阻止冒泡和默认粘贴行为
                         event.preventDefault();
                         event.stopPropagation();
                         // 获取粘贴数据
                         paste = clipboardData.getData('text') || '';
                         // 进行如下处理
-                        paste = $.telFilter(paste);
+                        // 除非是password，其他都过滤前后空格
+                        if (type != 'password') {
+                            paste = paste.trim();
+                        }
+                        // 邮箱处理，可能会使用#代替@避免被爬虫抓取
+                        if (type == 'email') {
+                            paste = paste.replace('#', '@');
+                        } else if (type == 'tel') {
+                            // 手机号处理
+                            paste = $.telFilter(paste);
+                        }
+
                         // 插入
                         this.value = paste;
                     }
