f6935cfaae28a35072194224d34793f4795837b7
zhangxinxu <zhangxinxu@zhangxinxu.com>
2019-01-30 20:42:34 +0800

    add js-concat  task
    add js-concat  task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 //清空文件
 var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 
 // 或者之前的modern
 var theme = 'peak';
@@ -12,6 +13,18 @@ var path = {
     'js': './theme/' + theme + '/js'
 };
 
+var fileUiJs = ['Keyboard.js', 'Follow.js', 'Enhance.js', 'Tab.js', 'Drop.js', 'DropList.js', 'DropPanel.js', 'Tips.js', 'LightTip.js', 'ErrorTip.js', 'Loading.js', 'Dialog.js', 'Datalist.js', 'Select.js', 'DateTime.js', 'Color.js', 'Range.js', 'Placeholder.js', 'Radio.js', 'Checkbox.js', 'Validate.js', 'Pagination.js'];
+var fileCompJs = ['Form.js', 'Table.js'];
+
+// 组装需要合并的JS
+var fileJS = [];
+fileUiJs.forEach(function (filename) {
+    fileJS.push(path.js + '/common/ui/' + filename);
+});
+fileCompJs.forEach(function (filename) {
+    fileJS.push(path.js + '/common/comp/' + filename);
+});
+
 // 清空CSS
 gulp.task('clean', function() {
     gulp.src([path.css + '/**/*.css'], {read: false})
@@ -19,10 +32,16 @@ gulp.task('clean', function() {
 });
 
 // 默认任务
-gulp.task('default', ['clean', 'sass:ui', 'sass:common', 'sass:comp'], function () {
+gulp.task('default', ['clean', 'sass:ui', 'sass:common', 'sass:comp', 'concat:js'], function () {
     gulp.start('sass:watch');
 });
 
+gulp.task('concat:js', function () {
+    console.log('合并所有JS');
+    return gulp.src(fileJS)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest(path.js + '/common'));
+});
 
 gulp.task('sass:ui', function () {
     console.log('编译sass:ui');
